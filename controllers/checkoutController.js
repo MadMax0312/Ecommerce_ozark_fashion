b140ef237d04da1eb1ac9326bcb766fb6365bd4d
max <thahirmuhammedap@gmail.com>
2023-11-01 22:02:48 +0530

    Checkoout started

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -2,17 +2,23 @@ const User = require("../models/userModel");
 const Product = require("../models/productModel");
 const Category = require("../models/categoryModel");
 const Address = require("../models/addressModel");
+const Cart = require("../models/cartModel");
 
-const loadCheckout = async(req, res) => {
+const loadCheckout = async (req, res) => {
+  try {
+    const userId = req.session.user_id;
+    console.log(userId);
+    const userData = await User.findById({_id:userId})
+    console.log(userData);
+    const userAddress = await Address.findOne({ userId });
+    const cartData = await Cart.findOne({ user_id: userId }).populate('items.product');
 
-  try{
-
-    res.render('checkout', {user: req.session.user_id});
-
-  }catch(error){
-    console.log(error.message);
+    res.render('checkout', { user: userData, address: userAddress, cart: cartData });
+  } catch (error) {
+    console.log(error.message);  
+    res.status(500).send('Internal Server Error'); 
   }
-}
+};
 
 module.exports = {
   loadCheckout
